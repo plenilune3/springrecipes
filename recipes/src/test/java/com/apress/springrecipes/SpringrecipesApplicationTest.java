760b641731a5f6fc871b3afc68e4e5e1ebcb9d7e
Mingyu Kang <plenilune095@gmail.com>
2020-12-19 09:36:41 +0900

    불필요한 테스트 코드 삭제

--- recipes/src/test/java/com/apress/springrecipes/SpringrecipesApplicationTest.java
+++ recipes/src/test/java/com/apress/springrecipes/SpringrecipesApplicationTest.java
@@ -1,6 +1,6 @@
 package com.apress.springrecipes;
 
-import com.apress.springrecipes.sequence.SequenceGenerator;
+import com.apress.springrecipes.sequence.SequenceDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,12 +8,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringrecipesApplicationTest {
 
-    @Autowired
-    SequenceGenerator generator;
-
-    @Test
-    void sequenceGeneratorTest() {
-        System.out.println(generator.getSequence());
-        System.out.println(generator.getSequence());
-    }
 }
