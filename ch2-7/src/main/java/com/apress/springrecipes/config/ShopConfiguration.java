38d32cb3899b2ff94f172bab3ccab58386bcc059
Mingyu Kang <plenilune095@gmail.com>
2020-12-23 23:58:51 +0900

    chapter 2-7. Resolve I18N Text Messages for Different Locales in Properties Files

---
+++ ch2-7/src/main/java/com/apress/springrecipes/config/ShopConfiguration.java
@@ -0,0 +1,19 @@
+package com.apress.springrecipes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ShopConfiguration {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:messages");
+        messageSource.setCacheSeconds(1);
+        return messageSource;
+    }
+
+}
