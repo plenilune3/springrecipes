7537bfcd272d43d578ee0b9bcdf3d1b5f7bc43da
plenilune3 <plenilune095@gmail.com>
2021-01-15 17:30:20 +0900

    init

--- ch2-1/src/main/java/com/apress/springrecipes/sequence/SequenceDao.java
+++
@@ -1,6 +0,0 @@
-package com.apress.springrecipes.sequence;
-
-public interface SequenceDao {
-    Sequence getSequence(String sequenceId);
-    int getNextValue(String sequenceId);
-}
--- recipes/src/test/java/com/apress/springrecipes/SpringrecipesApplicationTest.java
+++ recipes/src/test/java/com/apress/springrecipes/SpringrecipesApplicationTest.java
@@ -1,8 +1,5 @@
 package com.apress.springrecipes;
 
-import com.apress.springrecipes.sequence.SequenceDao;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
