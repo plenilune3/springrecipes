a198d24004e4112f45f81fc3136ffc04e8d4a32b
plenilune3 <plenilune095@gmail.com>
2021-01-20 20:25:56 +0900

    chapter 2-14. Access the Join Point information

--- calculator/src/main/java/com/apress/springrecipes/calculator/CalculatorLoggingAspect.java
+++ calculator/src/main/java/com/apress/springrecipes/calculator/CalculatorLoggingAspect.java
@@ -15,53 +15,29 @@ public class CalculatorLoggingAspect {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
-    @Around("execution(* *.*(..))")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("The method " + joinPoint.getSignature().getName() +
-                "() begins with " + Arrays.toString(joinPoint.getArgs()));
-
-        try {
-            Object result = joinPoint.proceed();
-            log.info("Ter method " + joinPoint.getSignature().getName() +
-                "() ends with " + result);
-            return result;
-        } catch (IllegalArgumentException e) {
-            log.error("An exception " + e + " has been thrown in " + joinPoint.getSignature().getName() + "()");
-            throw e;
-        }
+    @Before("execution(* *.*(..))")
+    public void logJointPoint(JoinPoint joinPoint) {
+        log.info("Join point kind : " + joinPoint.getKind());
+        log.info("Signature declaring type : " + joinPoint.getSignature().getDeclaringTypeName());
+        log.info("Signature name : " + joinPoint.getSignature().getName());
+        log.info("Arguments : " + Arrays.toString(joinPoint.getArgs()));
+        log.info("Target class : " + joinPoint.getTarget().getClass().getName());
+        log.info("This class : " + joinPoint.getThis().getClass().getName());
     }
 
-//    @Before("execution(* ArithmeticCalculator.add(..))")
-//    public void logBefore() {
-//        log.info("The method add() begins");
-//    }
-
-//    @Before("execution(* *.*(..))")
-//    public void logBefore(JoinPoint joinPoint) {
+//    @Around("execution(* *.*(..))")
+//    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
 //        log.info("The method " + joinPoint.getSignature().getName() +
 //                "() begins with " + Arrays.toString(joinPoint.getArgs()));
 //
-//    }
-//
-//    @After("execution(* *.*(..))")
-//    public void logAfter(JoinPoint joinPoint) {
-//        log.info("The method " + joinPoint.getSignature().getName() +
-//                "() ends");
-//    }
-//
-//    @AfterReturning(
-//            pointcut = "execution(* *.*(..))",
-//            returning = "result")
-//    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-//        log.info("Ter method " + joinPoint.getSignature().getName() +
+//        try {
+//            Object result = joinPoint.proceed();
+//            log.info("Ter method " + joinPoint.getSignature().getName() +
 //                "() ends with " + result);
-//    }
-//
-//    @AfterThrowing(
-//            pointcut = "execution(* *.*(..))",
-//            throwing = "e"
-//    )
-//    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-//        log.error("An exception " + e + " has been thrown in " + joinPoint.getSignature().getName() + "()");
+//            return result;
+//        } catch (IllegalArgumentException e) {
+//            log.error("An exception " + e + " has been thrown in " + joinPoint.getSignature().getName() + "()");
+//            throw e;
+//        }
 //    }
 }
